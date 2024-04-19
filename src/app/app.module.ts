0b5d50d61b29e206f582dc6fe5bebcbabb76143c
Soundaryas123 <soundaryas123@gmail.com>
2024-04-19 15:17:18 +0530

    header updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ResumeComponent } from './resume/resume.component';
@@ -17,18 +16,19 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { FormsModule } from '@angular/forms';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent,
     HomeComponent,
     PortfolioComponent,
     ResumeComponent,
     ContactComponent,
     ProjectCardComponent,
-    ProjectModalComponent
+    ProjectModalComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+constructor(private router:Router){
+  if (this.router.url === '/') {
+    this.router.navigateByUrl('/home');
+  }
+}
+  navigationItems = [
+    { label: 'Home', link: '/home', },
+    { label: 'Portfolio', link: '/portfolio',},
+    { label: 'Resume', link: '/resume', },
+    { label: 'Contact', link: '/contact',  }
+  ];
+
+  isActive(link: string): boolean {
+    return this.router.url === link;
+  }
 
 }
